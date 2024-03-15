2c3d86cb3fa65e70c926a6d9353de1cd80da58d6
Reza <raminizade@gmail.com>
2024-03-15 15:52:43 +0330

    some change in error file for show better items in responsive design

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -9,8 +9,8 @@ export default function Error({
 }) {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h2 className="text-xl">خطایی پیش آمده لطفا مجدد تلاش کنید!</h2>
-      <button className="text-white bg-red-500  p-3 shadow-sm  rounded-lg" onClick={() => reset()}>تلاش مجدد</button>
+      <h2 className="text-lg sm:text-xl">خطایی پیش آمده لطفا مجدد تلاش کنید!</h2>
+      <button className="text-white bg-red-500  p-2 sm:p-3 shadow-sm  rounded-lg" onClick={() => reset()}>تلاش مجدد</button>
     </div>
   )
 }
